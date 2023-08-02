f632b7a9ec1af14bd40b353cf3c827486bb104c2
Daniel <daniel@gmail.com>
2023-08-02 12:06:58 +0300

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,17 +9,8 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	 // If node is NULL, return 0
-	if (node == NULL)
-       	{
-            return 0;
-        }
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
 
-        // Check if the node has no left and right children
-        if (node->left == NULL && node->right == NULL)
-       	{
-            return 1; // Node is a leaf
-        } else {
-            return 0; // Node is not a leaf
-        }
+	return (1);
 }
