d6111f787d2e1a280289b07821977a312e64e320
Daniel <daniel@gmail.com>
2023-08-02 12:58:52 +0300

    5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,13 +9,16 @@
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL){
+	if (node == NULL)
+	{
 		return (0);
 	}
 	
-	if (node->parent == NULL){
+	if (node->parent == NULL)
+	{
 		return (1);
-	} else {
+	} else
+       	{
 		return (0);
 	}
 }
